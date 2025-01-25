1193d39310dd6a8bb5e2c03cbea4c801f1121f03
Brijesh <rbrijesh009@gmail.com>
2025-01-25 13:34:03 -0800

    Build process done

--- Loadr-Backend-main/routes/send.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const SendController = require('../controllers/send');
-
-router.post('/', SendController.send);
-
-module.exports = router;
--- Loadr-Frontend-main-3/README.md
+++
@@ -1,2 +0,0 @@
-# Loadr-Frontend
- 
